a0f32d3e3ad88742a1627234b438c0cd002f2ccd
ramon <ramonsilvab05@gmail.com>
2024-08-16 11:46:11 -0300

    removendo arquivos inutilizaveis

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,8 @@
+import logo from '../../assets/images/logo.png'
+
+export const Header = () => (
+  <div>
+    <h1>Olá testando a div Header</h1>
+    <img src={logo} alt="logo do restaurant" />
+  </div>
+)
