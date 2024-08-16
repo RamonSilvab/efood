7e8d85c73a42f53c2699dfa628efe943491ffbf2
ramon <ramonsilvab05@gmail.com>
2024-08-16 11:56:30 -0300

    resetando a config inicial

--- src/components/Header/index.tsx
+++
@@ -1,8 +0,0 @@
-import logo from '../../assets/images/logo.png'
-
-export const Header = () => (
-  <div>
-    <h1>Olá testando a div Header</h1>
-    <img src={logo} alt="logo do restaurant" />
-  </div>
-)
